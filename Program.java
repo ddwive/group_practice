a1d1dc3ceb2ad0fe7b7c7e6c6b0451a4d7b0755c
ddwive <85966432+ddwive@users.noreply.github.com>
2021-06-16 07:23:53 -0500

    Add files via upload
    added 'program' file

---
+++ Program.java
@@ -0,0 +1,14 @@
+import java.lang.System;
+import java.util.Scanner;
+
+class Program
+{
+    public static void main(String args[])
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is your message?");
+        String message = input.nextLine();
+
+        System.out.println("Your message is \"" + message + "\"!");
+    }
+}
